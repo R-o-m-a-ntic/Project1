100589fa8916cd26842c3ca3d8d7916431742717
User <alttokillall@proton.me>
2023-06-28 02:54:34 +0800

    Added unbreakable bonds

--- Whatevs.js
+++ Whatevs.js
@@ -3,12 +3,12 @@ const ChatID = '10971518';
 const Auth = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjg4MzQwMjc3LCJpYXQiOjE2ODc3NDAyNzcsInN1YiI6IjdhMGQwZDY3LWE4ODgtNDdkZi04ZDY0LThlODI3NjYwOTdlMCIsImVtYWlsIjoiYWE5NDc3NDAyNjlAYmVhY29ubWVzc2VuZ2VyLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwiLCJwcm92aWRlcnMiOlsiZW1haWwiXX0sInVzZXJfbWV0YWRhdGEiOnt9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6Im90cCIsInRpbWVzdGFtcCI6MTY4Nzc0MDI3N31dLCJzZXNzaW9uX2lkIjoiOGE5NTdlMjEtMzNjNi00MWJiLWI3ZTctY2QxMmQyOWZjYWZhIn0.sZtXmTivNnoG5F4BsjckQW9esY5YGGXo5LHJnJ7ztG4";
 const ErrorNone = 200;
 const ErrorTooFew = 1000;
-sendPostRequest();
+setTimeout(() => { sendPostRequest(); }, 70000);
   
   async function sendPostRequest() {
     let errorcount = 0;
   
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 7; i++) {
       setTimeout(async () => {
         try {
           const ci = i;
@@ -16,7 +16,7 @@ sendPostRequest();
           const is_main = !is_bot;
   
           await axios.post(`https://miguel.janitorai.com/chats/${ChatID}/messages`, {
-            message: "1",
+            message: "Crazy? I was crazy once.",
             is_bot,
             is_main,
           }, {
@@ -27,15 +27,15 @@ sendPostRequest();
           });
   
           console.log(ci);
-          if (ci >= 49) console.log("Done with batch.");
+          if (ci >= 6) console.log("Done with batch.");
         } catch (error) {
           errorcount++;
-          console.log(`Error occured! ${errorcount} errors out of 50`);
+          console.log(`Error occured! ${errorcount} errors out of 7`);
         }
-      }, i *10);
+      }, i *7000);
     }
   
-    setTimeout(sendPostRequest, errorcount ? (30 < errorcount ? 6000 : ErrorTooFew) : ErrorNone);
+    setTimeout(sendPostRequest, errorcount ? (5 < errorcount ? 6000 : ErrorTooFew) : ErrorNone);
   }
   
   sendPostRequest();
