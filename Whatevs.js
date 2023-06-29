febb725a614dd4859104f7ab03315643b4d390b7
User <alttokillall@proton.me>
2023-06-29 12:17:21 +0800

    Added something better

--- Whatevs.js
+++ Whatevs.js
@@ -1,14 +1,31 @@
 const axios = require('axios');
-const ChatID = '10971518';
-const Auth = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjg4MzQwMjc3LCJpYXQiOjE2ODc3NDAyNzcsInN1YiI6IjdhMGQwZDY3LWE4ODgtNDdkZi04ZDY0LThlODI3NjYwOTdlMCIsImVtYWlsIjoiYWE5NDc3NDAyNjlAYmVhY29ubWVzc2VuZ2VyLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwiLCJwcm92aWRlcnMiOlsiZW1haWwiXX0sInVzZXJfbWV0YWRhdGEiOnt9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6Im90cCIsInRpbWVzdGFtcCI6MTY4Nzc0MDI3N31dLCJzZXNzaW9uX2lkIjoiOGE5NTdlMjEtMzNjNi00MWJiLWI3ZTctY2QxMmQyOWZjYWZhIn0.sZtXmTivNnoG5F4BsjckQW9esY5YGGXo5LHJnJ7ztG4";
+const puppeteer = require('puppeteer');
+const ChatID = '11897986';
+const Auth = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjg4NjA5MDU0LCJpYXQiOjE2ODgwMDkwNTQsInN1YiI6IjBmYTgxMTU5LTQ5NDItNDExYi04YjRmLTQwYTcyMTFkNjViYSIsImVtYWlsIjoiZTE2ZDc3NDA0MzRAbGFtYXN0aWNvdHMuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6e30sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoicGFzc3dvcmQiLCJ0aW1lc3RhbXAiOjE2ODgwMDkwNTR9XSwic2Vzc2lvbl9pZCI6IjI2M2I0ZmM0LTUwZDUtNDMyZC04MTM0LTQ3ZDQ5MGQ2OThmNSJ9.saNw5k-a3YEvjZ_4_HG_oLBGnoAEBMV91G708jlpL1U";
 const ErrorNone = 200;
 const ErrorTooFew = 1000;
-setTimeout(() => { sendPostRequest(); }, 70000);
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.janitorai.com');
+
+  async function mainLoop() {
+    await sendPostRequest();
+    console.log("Finished spamming! Cooldown time...");
+
+    setTimeout(mainLoop, 6000);
+  }
+
+  mainLoop();
+
+})();
   
   async function sendPostRequest() {
     let errorcount = 0;
   
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 50; i++) {
       setTimeout(async () => {
         try {
           const ci = i;
@@ -16,7 +33,7 @@ setTimeout(() => { sendPostRequest(); }, 70000);
           const is_main = !is_bot;
   
           await axios.post(`https://miguel.janitorai.com/chats/${ChatID}/messages`, {
-            message: "Crazy? I was crazy once.",
+            message: "1",
             is_bot,
             is_main,
           }, {
@@ -27,15 +44,15 @@ setTimeout(() => { sendPostRequest(); }, 70000);
           });
   
           console.log(ci);
-          if (ci >= 6) console.log("Done with batch.");
+          if (ci >= 49) console.log("Done with batch.");
         } catch (error) {
           errorcount++;
-          console.log(`Error occured! ${errorcount} errors out of 7`);
+          console.log(`Error occured! ${errorcount} errors out of 50`);
         }
-      }, i *7000);
+      }, i *10);
     }
   
-    setTimeout(sendPostRequest, errorcount ? (5 < errorcount ? 6000 : ErrorTooFew) : ErrorNone);
+    setTimeout(sendPostRequest, errorcount ? (30 < errorcount ? 6000 : ErrorTooFew) : ErrorNone);
   }
   
   sendPostRequest();
